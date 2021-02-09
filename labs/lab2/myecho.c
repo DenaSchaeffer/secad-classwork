b6387bc1772529391c68afc3558bd4f20720490e
Dena Schaeffer <backd1@udayton.edu>
2021-02-08 22:39:45 -0500

    Revised myecho0.c

--- labs/lab2/myecho.c
+++ labs/lab2/myecho.c
@@ -1,8 +1,11 @@
 /* include libraries */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main (int argc, char *argv[])
 {
-   printf("%s\n",argv[1]);
+	char buffer[126];
+	strcpy(buffer,argv[1]);
+    printf("%s\n", buffer);
 }
